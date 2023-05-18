ac90e444371125019466d3f0e3f55e08ebb1bb5f
Ludvig <ludvig.leijel@skola.taby.se>
2023-05-18 21:46:44 +0200

    massa bilder

---
+++ javascript/products.js
@@ -0,0 +1,112 @@
+[
+  {
+    name: "Grand Document Bag - Black",
+    description:
+      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+    image: "../img/products/grand document bag black.png",
+    type: "Shoulder bag",
+    dimensions: { height: 20, width: 30, depth: 15 },
+    weight: 2,
+    extraImages: [],
+  },
+  {
+    name: "Grand Document Bag - Brown",
+    description:
+      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+    image: "../img/products/grand document bag brown.png",
+    type: "Shoulder bag",
+    dimensions: { height: 20, width: 30, depth: 15 },
+    weight: 2,
+    extraImages: [],
+  },
+  {
+    name: "Havana Rucksack Bag - Black",
+    description:
+      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+    image: "../img/products/Havana Rucksack Bag - Black.png",
+    type: "Backpack",
+    dimensions: { height: 20, width: 30, depth: 15 },
+    weight: 2,
+    extraImages: [],
+  },
+  {
+    name: "Havana Shoulder Bag",
+    description:
+      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+    image: "../img/products/Havana Shoulder Bag.png",
+    type: "Shoulder bag",
+    dimensions: { height: 20, width: 30, depth: 15 },
+    weight: 2,
+    extraImages: [],
+  },
+  {
+    name: "Havana Vintage Crossbody Messenger Bag",
+    description:
+      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+    image: "../img/products/Havana Vintage Crossbody Messenger Bag.png",
+    type: "Shoulder bag",
+    dimensions: { height: 20, width: 30, depth: 15 },
+    weight: 2,
+    extraImages: [],
+  },
+  {
+    name: "Khaki Large Vintage Shoulder Bag",
+    description:
+      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+    image: "../img/products/Khaki Large Vintage Shoulder Bag.png",
+    type: "Shoulder bag",
+    dimensions: { height: 20, width: 30, depth: 15 },
+    weight: 2,
+    extraImages: [],
+  },
+  {
+    name: "Khaki Shoulder Bag",
+    description:
+      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+    image: "../img/products/Khaki Shoulder Bag.png",
+    type: "Shoulder bag",
+    dimensions: { height: 20, width: 30, depth: 15 },
+    weight: 2,
+    extraImages: [],
+  },
+  {
+    name: "Khaki Vintage Crossbody Messenger Bag",
+    description:
+      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+    image: "../img/products/Khaki Vintage Crossbody Messengar Bag.png",
+    type: "Shoulder bag",
+    dimensions: { height: 20, width: 30, depth: 15 },
+    weight: 2,
+    extraImages: [],
+  },
+  {
+    name: "Messenger Bag - Black",
+    description:
+      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+    image: "../img/products/Messenger Bag - Black.png",
+    type: "Shoulder bag",
+    dimensions: { height: 20, width: 30, depth: 15 },
+    weight: 2,
+    extraImages: [],
+  },
+  {
+    name: "Messenger Bag - Brown",
+    description:
+      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+    image: "../img/products/Messenger Bag Brown.png",
+    type: "Shoulder bag",
+    dimensions: { height: 20, width: 30, depth: 15 },
+    weight: 2,
+    extraImages: [],
+  },
+  {
+    name: "Khaki Rucksack Large",
+    description:
+      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+    image: "../img/products/Rucksack Large - Khaki Canvas.png",
+    type: "Shoulder bag",
+    dimensions: { height: 20, width: 30, depth: 15 },
+    weight: 2,
+    extraImages: [],
+  },
+];
