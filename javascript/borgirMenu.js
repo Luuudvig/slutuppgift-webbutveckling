aa4ed94b03bcd7f9cc2129ca081e84adf6833c70
Ludvig <ludvig.leijel@skola.taby.se>
2023-05-19 18:12:04 +0200

    aaaaaaaaaaaaaaaaa

--- javascript/borgirMenu.js
+++ javascript/borgirMenu.js
@@ -1,5 +1,9 @@
+const htmlScroll = document.getElementById("html");
+
 const burgerButton = document.getElementById("hamburger-menu-button");
 const burgerMenu = document.getElementById("hamburger-menu");
+
 burgerButton.addEventListener("click", () => {
   burgerMenu.classList.toggle("display-flex");
+  htmlScroll.classList.toggle("noScroll");
 });
