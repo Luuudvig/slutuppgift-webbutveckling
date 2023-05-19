311ad3216cb3b8f18962e378144a261f2f877f48
Ludvig <ludvig.leijel@skola.taby.se>
2023-05-19 10:30:56 +0200

    js

--- javascript/products.js
+++ javascript/products.js
@@ -2,122 +2,133 @@ const products = [
   {
     name: "Grand Document Bag - Black",
     description:
-      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+      "Praesent feugiat metus mi, in sagittis elit ultricies a. Sed eget faucibus arcu, non rutrum nibh. Cras sed pellentesque enim, ut placerat neque. Phasellus in enim et tortor auctor auctor. Vestibulum eget pharetra erat, ut fermentum nunc. Etiam aliquam ante blandit mattis aliquet. Maecenas nec molestie odio, ut hendrerit nisi. Morbi vehicula molestie est et varius.",
     image: "../img/products/grand document bag black.png",
     price: 199,
     type: "Shoulder bag",
     dimensions: { height: 20, width: 30, depth: 15 },
     weight: 2,
+    color: "Black",
     extraImages: [],
   },
   {
     name: "Grand Document Bag - Brown",
     description:
-      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+      "Praesent feugiat metus mi, in sagittis elit ultricies a. Sed eget faucibus arcu, non rutrum nibh. Cras sed pellentesque enim, ut placerat neque. Phasellus in enim et tortor auctor auctor. Vestibulum eget pharetra erat, ut fermentum nunc. Etiam aliquam ante blandit mattis aliquet. Maecenas nec molestie odio, ut hendrerit nisi. Morbi vehicula molestie est et varius.",
     image: "../img/products/grand document bag brown.png",
     price: 199,
     type: "Shoulder bag",
     dimensions: { height: 20, width: 30, depth: 15 },
     weight: 2,
+    color: "Black",
     extraImages: [],
   },
   {
     name: "Havana Rucksack Bag - Black",
     description:
-      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+      "Praesent feugiat metus mi, in sagittis elit ultricies a. Sed eget faucibus arcu, non rutrum nibh. Cras sed pellentesque enim, ut placerat neque. Phasellus in enim et tortor auctor auctor. Vestibulum eget pharetra erat, ut fermentum nunc. Etiam aliquam ante blandit mattis aliquet. Maecenas nec molestie odio, ut hendrerit nisi. Morbi vehicula molestie est et varius.",
     image: "../img/products/Havana Rucksack Bag - Black.png",
     price: 199,
     type: "Backpack",
     dimensions: { height: 20, width: 30, depth: 15 },
     weight: 2,
+    color: "Black",
     extraImages: [],
   },
   {
     name: "Havana Shoulder Bag",
     description:
-      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+      "Praesent feugiat metus mi, in sagittis elit ultricies a. Sed eget faucibus arcu, non rutrum nibh. Cras sed pellentesque enim, ut placerat neque. Phasellus in enim et tortor auctor auctor. Vestibulum eget pharetra erat, ut fermentum nunc. Etiam aliquam ante blandit mattis aliquet. Maecenas nec molestie odio, ut hendrerit nisi. Morbi vehicula molestie est et varius.",
     image: "../img/products/Havana Shoulder Bag.png",
     price: 199,
     type: "Shoulder bag",
     dimensions: { height: 20, width: 30, depth: 15 },
     weight: 2,
+    color: "Black",
     extraImages: [],
   },
   {
     name: "Havana Vintage Messenger Bag",
     description:
-      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+      "Praesent feugiat metus mi, in sagittis elit ultricies a. Sed eget faucibus arcu, non rutrum nibh. Cras sed pellentesque enim, ut placerat neque. Phasellus in enim et tortor auctor auctor. Vestibulum eget pharetra erat, ut fermentum nunc. Etiam aliquam ante blandit mattis aliquet. Maecenas nec molestie odio, ut hendrerit nisi. Morbi vehicula molestie est et varius.",
     image: "../img/products/Havana Vintage Crossbody Messenger Bag.png",
     price: 199,
     type: "Shoulder bag",
     dimensions: { height: 20, width: 30, depth: 15 },
     weight: 2,
+    color: "Black",
     extraImages: [],
   },
   {
     name: "Khaki Large Vintage Shoulder Bag",
     description:
-      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+      "Praesent feugiat metus mi, in sagittis elit ultricies a. Sed eget faucibus arcu, non rutrum nibh. Cras sed pellentesque enim, ut placerat neque. Phasellus in enim et tortor auctor auctor. Vestibulum eget pharetra erat, ut fermentum nunc. Etiam aliquam ante blandit mattis aliquet. Maecenas nec molestie odio, ut hendrerit nisi. Morbi vehicula molestie est et varius.",
     image: "../img/products/Khaki Large Vintage Shoulder Bag.png",
     price: 199,
     type: "Shoulder bag",
     dimensions: { height: 20, width: 30, depth: 15 },
     weight: 2,
+    color: "Black",
     extraImages: [],
   },
   {
     name: "Khaki Shoulder Bag",
     description:
-      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+      "Praesent feugiat metus mi, in sagittis elit ultricies a. Sed eget faucibus arcu, non rutrum nibh. Cras sed pellentesque enim, ut placerat neque. Phasellus in enim et tortor auctor auctor. Vestibulum eget pharetra erat, ut fermentum nunc. Etiam aliquam ante blandit mattis aliquet. Maecenas nec molestie odio, ut hendrerit nisi. Morbi vehicula molestie est et varius.",
     image: "../img/products/Khaki Shoulder Bag.png",
     price: 199,
     type: "Shoulder bag",
     dimensions: { height: 20, width: 30, depth: 15 },
     weight: 2,
+    color: "Black",
     extraImages: [],
   },
   {
     name: "Khaki Vintage Crossbody Bag",
     description:
-      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+      "Praesent feugiat metus mi, in sagittis elit ultricies a. Sed eget faucibus arcu, non rutrum nibh. Cras sed pellentesque enim, ut placerat neque. Phasellus in enim et tortor auctor auctor. Vestibulum eget pharetra erat, ut fermentum nunc. Etiam aliquam ante blandit mattis aliquet. Maecenas nec molestie odio, ut hendrerit nisi. Morbi vehicula molestie est et varius.",
     image: "../img/products/Khaki Vintage Crossbody Messengar Bag.png",
     price: 199,
     type: "Shoulder bag",
     dimensions: { height: 20, width: 30, depth: 15 },
     weight: 2,
+    color: "Black",
     extraImages: [],
   },
   {
     name: "Messenger Bag - Black",
     description:
-      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+      "Praesent feugiat metus mi, in sagittis elit ultricies a. Sed eget faucibus arcu, non rutrum nibh. Cras sed pellentesque enim, ut placerat neque. Phasellus in enim et tortor auctor auctor. Vestibulum eget pharetra erat, ut fermentum nunc. Etiam aliquam ante blandit mattis aliquet. Maecenas nec molestie odio, ut hendrerit nisi. Morbi vehicula molestie est et varius.",
     image: "../img/products/Messenger Bag - Black.png",
     price: 199,
     type: "Shoulder bag",
     dimensions: { height: 20, width: 30, depth: 15 },
     weight: 2,
+    color: "Black",
     extraImages: [],
   },
   {
     name: "Messenger Bag - Brown",
     description:
-      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+      "Praesent feugiat metus mi, in sagittis elit ultricies a. Sed eget faucibus arcu, non rutrum nibh. Cras sed pellentesque enim, ut placerat neque. Phasellus in enim et tortor auctor auctor. Vestibulum eget pharetra erat, ut fermentum nunc. Etiam aliquam ante blandit mattis aliquet. Maecenas nec molestie odio, ut hendrerit nisi. Morbi vehicula molestie est et varius.",
     image: "../img/products/Messenger Bag Brown.png",
     price: 199,
     type: "Shoulder bag",
     dimensions: { height: 20, width: 30, depth: 15 },
     weight: 2,
+    color: "Black",
     extraImages: [],
   },
   {
     name: "Khaki Rucksack Large",
     description:
-      "ras nec dui facilisis, consectetur nunc vitae, gravida nulla. Suspendisse non metus sit amet metus ultrices tristique sed fermentum felis. Morbi vitae congue sapien, vel pulvinar urna. In quis tempus orci, a condimentum urna. Phasellus commodo augue pharetra, faucibus dolor eu, sollicitudin tortor. Nulla lobortis, mauris non ultricies sodales, tellus sapien vehicula quam, at porttitor dui libero eu leo. Aliquam arcu tortor, pharetra at sagittis et, pulvinar a leo. Nunc ut tortor vel dolor sagittis elementum a quis velit. Aenean varius convallis eleifend.",
+      "Praesent feugiat metus mi, in sagittis elit ultricies a. Sed eget faucibus arcu, non rutrum nibh. Cras sed pellentesque enim, ut placerat neque. Phasellus in enim et tortor auctor auctor. Vestibulum eget pharetra erat, ut fermentum nunc. Etiam aliquam ante blandit mattis aliquet. Maecenas nec molestie odio, ut hendrerit nisi. Morbi vehicula molestie est et varius.",
     image: "../img/products/Rucksack Large - Khaki Canvas.png",
     price: 199,
     type: "Backpack",
     dimensions: { height: 20, width: 30, depth: 15 },
     weight: 2,
+    color: "Black",
     extraImages: [],
   },
 ];
@@ -148,10 +159,42 @@ products.forEach((product) => {
   );
 });
 
-let productWindow = docment.getElementById("target-product");
+//Mer info skärm
+const productWindow = document.getElementById("target-product");
+const productItems = productGrid.getElementsByClassName("product-grid-item");
+for (let i = 0; i < productItems.length; i++) {
+  const productItem = productItems[i];
+  productItem.addEventListener("click", () => {
+    let productName = document.getElementById("large-product-name");
+    let productImage = document.getElementById("large-product-image");
+    let productType = document.getElementById("product-type");
+    let productPrice = document.getElementById("product-price");
+    let productDescription = document.getElementById("product-description");
+    let productHeight = document.getElementById("product-price");
+    let productWidth = document.getElementById("product-description");
+    let productDepth = document.getElementById("product-price");
+    let productWeight = document.getElementById("product-description");
+    let productColor = document.getElementById("product-color");
 
-let griditem = document.getElementsByClassName("product-grid-item");
-griditem.onclick = function (event) {
-  productWindow.style.display = "flex";
-  document.getElementsByTagName("html").style.overflow = "hidden";
-};
+    productName.innerHTML = products[i].name;
+    productImage.innerHTML = products[i].image;
+    productType.innerHTML = products[i].type;
+    productPrice.innerHTML = products[i].price;
+    productDescription.innerHTML = products[i].description;
+    productHeight.innerHTML = products[i].dimensions.height;
+    productWidth.innerHTML = products[i].dimensions.width;
+    productDepth = products[i].dimensions.depth;
+    productWeight.innerHTML = products[i].weight;
+    productColor.innerHTML = products[i].color;
+
+    productWindow.style.display = "flex";
+    document.getElementById("html").style.overflowY = "hidden";
+  });
+}
+
+//Stäng info skärmen
+const closeProductWindow = document.getElementById("close-target-window");
+closeProductWindow.addEventListener("click", () => {
+  productWindow.style.display = "none";
+  document.getElementById("html").style.overflowY = "scroll";
+});
